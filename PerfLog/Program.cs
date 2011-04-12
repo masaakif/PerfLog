33aa2f380260d96f161fa0e5720ddeb0914a608b
masaakif <masaakif.github@gmail.com>
2011-04-12 15:35:30 +0900

    First Version

---
+++ PerfLog/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace PerfLog
+{
+    public class ProcessMonitor
+    {
+        WindowsAPIs.Utils u = new WindowsAPIs.Utils();
+
+        public void output(String pname)
+        {
+            Process[] ps = Process.GetProcesses();
+            String title = "Process Name (ID)\tWorking Size\tThread\tHandle\tGDI\tUser\tSTime";
+            Console.WriteLine(title);
+
+            foreach (Process p in ps)
+            {
+                if (p.ProcessName.IndexOf(pname) != -1)
+                {
+                    String msg = p.ProcessName + "(" + p.Id.ToString() + ")";
+                    msg += "\t" + p.WorkingSet64.ToString()
+                         + "\t" + p.Threads.Count.ToString()
+                         + "\t" + p.HandleCount.ToString()
+                         + "\t" + u.GetGDIObjects((uint)p.Id).ToString()
+                         + "\t" + u.GetUserObjects((uint)p.Id).ToString()
+                         + "\t" + p.StartTime.ToShortTimeString();
+                         
+                    Console.WriteLine(msg+"\n");
+                }
+
+
+
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessMonitor pm = new ProcessMonitor();
+            if (args.Count() == 0)
+            {
+                Console.WriteLine("Usage : PerfLog 'Process Name' [-I=MilliSeconds (default = 2000)]");
+                return;
+            }
+
+            int intval = 2000;
+            String pname = "";
+
+            foreach (String arg in args)
+            {
+                if (-1 < arg.IndexOf("-I="))
+                {
+                    intval = int.Parse(arg.Substring(3));
+                }
+                else
+                {
+                    pname = arg;
+                }
+            }
+
+            if (pname == "")
+            {
+                Console.WriteLine("Process name should be given!! PerfLog exitted...");
+                return;
+            }
+
+            while (true)
+            {
+                Console.WriteLine(DateTime.Now.ToLongTimeString()); 
+                pm.output(args[0]);
+                System.Threading.Thread.Sleep(intval);
+            }
+        }
+    }
+}
---
+++ PerfLog/Utils.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace WindowsAPIs
+{
+    class Utils
+    {
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetActiveWindow();
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("User32.Dll", CharSet = CharSet.Unicode)]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder s, int nMaxCount);
+
+        [DllImport("user32.dll")]
+        private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr OpenProcess(UInt32 dwDesiredAccess, Int32 bInheritHandle, UInt32 dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool CloseHandle(IntPtr hObject);
+
+        [DllImport("user32.dll")]
+        private static extern uint GetGuiResources(IntPtr hObject, uint uiFlags);
+
+        [DllImport("psapi.dll")]
+        private static extern uint GetModuleFileNameEx(IntPtr hProcess, IntPtr hModule, [Out] StringBuilder lpBaseName, [In] [MarshalAs(UnmanagedType.U4)] int nSize);
+
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern bool BringWindowToTop(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32", CharSet = CharSet.Auto)]
+        private static extern int GetWindowLong(IntPtr hWnd, int index);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern uint GetWindowLongPtr(IntPtr hWnd, int index);
+        //private static extern IntPtr GetWindowLongPtr(IntPtr hWnd, int index);
+
+        private const uint WS_VISIBLE = 0x10000000;
+        private const uint WS_THICKFRAME = 0x00040000;
+        private const uint WS_BORDER = 0x00800000;
+        private const uint WS_DLGFRAME = 0x00400000;
+        private const int GWL_STYLE = -16;
+        public int SW_RESTORE = 9;
+
+        private const UInt32 PROCESS_QUERY_INFORMATION = 0x0400;
+        private const uint GR_GDIOBJECTS = 0;
+        private const uint GR_USEROBJECTS = 1;
+
+        public string className = "";
+        public string title = "";
+        public string command = "";
+        public string arguments = "";
+        public string verbose = "false";
+
+        public bool isRunning = false;
+        
+        public bool hasGUI(IntPtr hwnd)
+        {
+            uint style = (uint)GetWindowStyle(hwnd);
+            uint mixStyle = (WS_VISIBLE | WS_BORDER | WS_DLGFRAME);
+
+            if ((style & mixStyle) == mixStyle)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        // http://hongliang.seesaa.net/article/148698461.html のコードを利用しました
+        private uint GetWindowStyle(IntPtr hWnd)
+        {
+            // 32bitプロセスか64bitプロセスかで呼び出すAPIが変わる
+            if (IntPtr.Size == 4)
+                return (uint)(GetWindowLong(hWnd, GWL_STYLE));
+            else
+                return GetWindowLongPtr(hWnd, GWL_STYLE);
+        }
+
+        private Boolean IsExists(string filename)
+        {
+            if (System.IO.File.Exists(filename))
+            {
+                return true;
+            }
+
+            string[] paths = System.Environment.GetEnvironmentVariable("PATH").Split(';');
+            foreach (string path in paths)
+            {
+                if (System.IO.File.Exists(path + "\\" + filename))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void MoveWindowTop(IntPtr hWnd)
+        {
+            ShowWindowAsync(hWnd, SW_RESTORE);
+            SetForegroundWindow(hWnd);
+        }
+
+        public void OutputDatetime()
+        {
+            Debug.WriteLine(System.DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss.fff"));
+        }
+
+        public void ExecuteCommand()
+        {
+            //MessageBox.Show(System.Environment.GetEnvironmentVariable("PATH"));
+            if (verbose == "true")
+            {
+                string msg = "Class='" + className + "' ";
+                msg += "Title='"+ title + "' " ;
+                msg += "Command='"+ command + "' " ;
+                msg += "Arguments='"+ arguments + "' " ;
+                msg += "Verbose='" + verbose + "'";
+
+                MessageBox.Show(msg);
+            }
+
+            if (isRunning)
+            {
+                return;
+            }
+            if (command == "")
+            {
+                return;
+            }
+            if (IsExists(command) == false)
+            {
+                System.Windows.Forms.MessageBox.Show(command + " doesn't exist");
+                return;
+            }
+
+            Process p = new Process();
+            p.StartInfo.FileName = command;
+            p.StartInfo.Arguments = arguments;
+            p.Start();
+        }
+
+        private const int nChars = 1024;
+
+        public string getActiveWindowModuleName(bool isOnlyBinary = true)
+        {
+            StringBuilder sb = new StringBuilder(nChars);
+
+            IntPtr hWnd = GetForegroundWindow();
+            uint processId = 0;
+            GetWindowThreadProcessId(hWnd, out processId);
+            IntPtr hProcess = OpenProcess(1040, 0, processId);
+            GetModuleFileNameEx(hProcess, IntPtr.Zero, sb, nChars);
+            CloseHandle(hProcess);
+
+            if (isOnlyBinary)
+            {
+                string[] ary = sb.ToString().Split('\\');
+                return ary[ary.Length-1];
+            }
+
+            return sb.ToString();
+        }
+
+        public uint GetGDIObjects(UInt32 dwProcessId)
+        {
+            IntPtr hProcess = OpenProcess(PROCESS_QUERY_INFORMATION, -1, dwProcessId);
+            if (hProcess != null)
+            {
+                uint cnt = GetGuiResources(hProcess, GR_GDIOBJECTS);
+                CloseHandle(hProcess);
+                return cnt;
+            }
+            return 0;
+        }
+
+        public uint GetUserObjects(UInt32 dwProcessId)
+        {
+            IntPtr hProcess = OpenProcess(PROCESS_QUERY_INFORMATION, -1, dwProcessId);
+            if (hProcess != null)
+            {
+                uint cnt = GetGuiResources(hProcess, GR_USEROBJECTS);
+                CloseHandle(hProcess);
+                return cnt;
+            }
+            return 0;
+        }
+    }
+}
